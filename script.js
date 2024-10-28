ddb687885fbf93b24a01d9b7791fd1a77d93dd92
DarysFox <example@gmail.com>
2024-10-28 15:02:38 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+// let num = 4.6;
+
+// console.log(-4 / 0);
+
+// console.log("string" * 9);
+
+// const persone = "Alex";
+
+// const bool = true;
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarried: false,
+// };
+
+// console.log(obj.name);
+
+// let arr = ["plum.png", "orange.jpg", 6, "apple.bmp", {}, []];
+
+// console.log(arr[1]);
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("U are 18?", "18");
+// console.log(typeof answer);
+
+// const answers = [];
+
+// answers[0] = prompt("Whats ur name?", "");
+// answers[1] = prompt("Whats ur surname?", "");
+// answers[2] = prompt("How old r u?", "");
+
+// document.write(answers);
+// console.log(typeof answers);
+
+// console.log(typeof null);
+
+const category = "toys";
+console.log(`https//someurl.com/${category}/5`);
+
+const user = "Ivan";
+alert(`Hello, ${user}`);
+
+console.log("arr" + " - object");
+console.log(4 + "5");
+
+let incr = 10,
+  decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
+
+console.log(2 + 2 * 2 !== "6");
+
+const isChecked = false,
+  isClose = false;
+console.log(isChecked || !isClose);
