73ef52ab7c72b72177253634aa729f6c237af08d
DarysFox <example@gmail.com>
2024-11-02 15:02:41 +0300

    switch

--- script.js
+++ script.js
@@ -86,17 +86,36 @@
 
 // console.log(personalMovieDB);
 
-if (4 == 9) {
-  console.log("ok!");
-} else {
-  console.log("error");
-}
+// if (4 == 9) {
+//   console.log("ok!");
+// } else {
+//   console.log("error");
+// }
 
+// if (num < 49) {
+//   console.log("error");
+// } else if (num > 100) {
+//   console.log("a lot");
+// } else {
+//   console.log("ok!");
+// }
+
+// num === 50 ? console.log("ok!") : console.log("error");
 const num = 50;
-if (num < 49) {
-  console.log("error");
-} else if (num > 100) {
-  console.log("a lot");
-} else {
-  console.log("ok!");
+switch (num) {
+  case 49:
+    console.log("false");
+    break;
+
+  case 100:
+    console.log("false");
+    break;
+
+  case 50:
+    console.log("true");
+    break;
+
+  default:
+    console.log("not today");
+    break;
 }
