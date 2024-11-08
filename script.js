d31c0c000af7d8498cc3f8e1de63726b6cf8dba5
DarysFox <example@gmail.com>
2024-11-08 17:11:06 +0300

    continue first

--- script.js
+++ script.js
@@ -166,14 +166,25 @@
 //   console.log(i);
 // }
 
-let result = "";
-const length = 7;
+// let result = "";
+// const length = 7;
 
-for (let i = 1; i < length; i++) {
-  for (let j = 0; j < i; j++) {
-    result += "*";
+// for (let i = 1; i < length; i++) {
+//   for (let j = 0; j < i; j++) {
+//     result += "*";
+//   }
+//   result += "\n";
+// }
+
+// console.log(result);
+
+first: for (let i = 1; i < 3; i++) {
+  console.log(`first level:  ${i}`);
+  for (let j = 0; j < 3; j++) {
+    console.log(`second level:  ${j}`);
+    for (let k = 0; k < 3; k++) {
+      if (k === 2) continue first;
+      console.log(`third level:  ${k}`);
+    }
   }
-  result += "\n";
 }
-
-console.log(result);
